e59fbc03bc080052ed6fcda85a61508505f0a17b
Nanjunda Reddy G <nanjunda.reddy.g@ibm.com>
2021-08-26 09:14:21 +0530

    Selenium Activities commit

---
+++ Selenium/Activities/Activity4.java
@@ -0,0 +1,22 @@
+package Selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Activity4 {
+
+	    public static void main(String[] args) {
+	        WebDriver driver = new FirefoxDriver();
+	        driver.get("https://www.training-support.net");
+	        String title = driver.getTitle();
+	        System.out.println("Page title is: " + title);
+	        driver.findElement(By.xpath("/html/body/div/div/div/a")).click();
+	        System.out.println("Heading is: " + driver.getTitle());
+	        driver.close();
+
+	    }
+
+	}
+
+
